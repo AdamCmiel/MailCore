81b9c7bc566ebd2529c4ad978121b8cd61f9bf61
Adam Cmiel <adamcmiel@gmail.com>
2016-01-09 22:02:21 -0800

    Initial Commit

---
+++ MailCore/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MailCore
+//
+//  Created by Adam Cmiel on 1/9/16.
+//  Copyright © 2016 Adam Cmiel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
